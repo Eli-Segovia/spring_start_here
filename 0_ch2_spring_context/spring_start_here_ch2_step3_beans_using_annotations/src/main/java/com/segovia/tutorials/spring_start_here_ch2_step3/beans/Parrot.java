f8b088722a417c028474bb490863bb49dfac96ba
Eli <elisego46@gmail.com>
2026-04-10 13:55:46 -0500

    finish annotation demonstration

--- 0_ch2_spring_context/spring_start_here_ch2_step3_beans_using_annotations/src/main/java/com/segovia/tutorials/spring_start_here_ch2_step3/beans/Parrot.java
+++ 0_ch2_spring_context/spring_start_here_ch2_step3_beans_using_annotations/src/main/java/com/segovia/tutorials/spring_start_here_ch2_step3/beans/Parrot.java
@@ -1,5 +1,6 @@
 package com.segovia.tutorials.spring_start_here_ch2_step3.beans;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.stereotype.Component;
 
 /*
@@ -14,6 +15,16 @@ import org.springframework.stereotype.Component;
 public class Parrot {
 
     private String name;
+
+    /**
+     * Initializes this parrot with the name "Boba" after Spring has created this bean. Usually
+     * We would actually get this from a DB or a configuration file, but here we are just
+     * hardcoding for demo purposes
+     */
+    @PostConstruct
+    public void init() {
+        this.name = "Boba";
+    }
 
     public String getName() {
         return name;
