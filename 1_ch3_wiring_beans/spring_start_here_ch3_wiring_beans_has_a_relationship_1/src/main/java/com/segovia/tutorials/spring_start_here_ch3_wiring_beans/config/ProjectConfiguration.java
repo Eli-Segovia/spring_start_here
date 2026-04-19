42fdea055f710c2380085b90681cc20f68549232
Eli <elisego46@gmail.com>
2026-04-18 19:32:25 -0500

    change structure a lil; add ch3 example

---
+++ 1_ch3_wiring_beans/spring_start_here_ch3_wiring_beans_has_a_relationship_1/src/main/java/com/segovia/tutorials/spring_start_here_ch3_wiring_beans/config/ProjectConfiguration.java
@@ -0,0 +1,22 @@
+package com.segovia.tutorials.spring_start_here_ch3_wiring_beans.config;
+
+import com.segovia.tutorials.spring_start_here_ch3_wiring_beans.beans.Parrot;
+import com.segovia.tutorials.spring_start_here_ch3_wiring_beans.beans.Person;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ProjectConfiguration {
+
+    @Bean
+    public Parrot parrot() {
+        Parrot p = new Parrot("Koko");
+        return p;
+    }
+
+    @Bean
+    public Person person() {
+        Person p = new Person("Kingston");
+        return p;
+    }
+}
