5d8a99f479f4410a965b0e63d0b4bf2931cb7087
Eli <elisego46@gmail.com>
2026-04-18 19:04:31 -0500

    add programatic example

--- 0_ch2_spring_context/spring_start_here_ch2_step5_programmatically_adding_beans/src/main/java/com/segovia/tutorials/spring_start_here_ch2_step5/beans/Parrot.java
+++ 0_ch2_spring_context/spring_start_here_ch2_step5_programmatically_adding_beans/src/main/java/com/segovia/tutorials/spring_start_here_ch2_step5/beans/Parrot.java
@@ -1,13 +1,9 @@
 package com.segovia.tutorials.spring_start_here_ch2_step5.beans;
 
-import jakarta.annotation.PostConstruct;
-import org.springframework.stereotype.Component;
-
 /*
  * Dummy class that we will add  to our spring context
  * It is just a parrot.
  */
-@Component
 public class Parrot {
 
     private String name;
--- 0_ch2_spring_context/spring_start_here_ch2_step5_programmatically_adding_beans/src/main/java/com/segovia/tutorials/spring_start_here_ch2_step5/main/Main.java
+++ 0_ch2_spring_context/spring_start_here_ch2_step5_programmatically_adding_beans/src/main/java/com/segovia/tutorials/spring_start_here_ch2_step5/main/Main.java
@@ -4,20 +4,34 @@ import com.segovia.tutorials.spring_start_here_ch2_step5.beans.Parrot;
 import com.segovia.tutorials.spring_start_here_ch2_step5.configuration.ProjectConfiguration;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.GenericApplicationContext;
 
 public class Main {
 
     public static void main(String[] args) {
-        /*
-         * Here we have created our context and told Spring which Configuration to use
-         * That configuration has told Spring how to find the Parrot component
-         * It will pull this Parrot component and add it to the context.
-         */
-        ApplicationContext context = new AnnotationConfigApplicationContext(ProjectConfiguration.class);
 
-        Parrot nuestroPerico = context.getBean(Parrot.class);
+        // Need to use GenericApplicationContext to register beans programmatically
+        GenericApplicationContext context = new AnnotationConfigApplicationContext(ProjectConfiguration.class);
 
-        System.out.println(nuestroPerico.getName());
+        // instead of using annotations or beans, we create the instance, and we register it
+        // so that spring controls it.
+
+        Parrot bobaBird = new Parrot();
+        bobaBird.setName("Boba");
+
+        // we use registerBean to register the bean. It takes 4 arguments
+        //    - String                   -- the name we want to register the bean with
+        //    - Class<T>                 -- the class of the Bean (Parrot.class)
+        //    - Supplier<T>              -- supplies the instance of the Bean
+        //    - BeanDefinitionCustomizer -- a varargs argument that gives extra customizations. ignore for now
+        context.registerBean("bobaBean", Parrot.class, () -> bobaBird);
+
+        Parrot springBobaBird = context.getBean(Parrot.class);
+
+
+        System.out.println(springBobaBird.getName());
+
+
 
     }
 }
