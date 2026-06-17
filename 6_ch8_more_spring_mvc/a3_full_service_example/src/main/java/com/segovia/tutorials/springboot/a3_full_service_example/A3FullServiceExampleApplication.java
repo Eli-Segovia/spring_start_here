2cc0871c2cdb06067b7505f4f865cfbec8099ee7
Eli-Segovia <elisego46@gmail.com>
2026-06-16 19:02:10 -0500

    add full service project

---
+++ 6_ch8_more_spring_mvc/a3_full_service_example/src/main/java/com/segovia/tutorials/springboot/a3_full_service_example/A3FullServiceExampleApplication.java
@@ -0,0 +1,14 @@
+package com.segovia.tutorials.springboot.a3_full_service_example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class A3FullServiceExampleApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Hello world");
+		SpringApplication.run(A3FullServiceExampleApplication.class, args);
+	}
+
+}
---
+++ 6_ch8_more_spring_mvc/a3_full_service_example/src/test/java/com/segovia/tutorials/springboot/a3_full_service_example/A3FullServiceExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.segovia.tutorials.springboot.a3_full_service_example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class A3FullServiceExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
